6147499ad486bc02ec634f0c66174c76084038f0
Matthew Chang <matthewchang04@gmail.com>
2026-01-27 20:06:52 -0500

    setting up for options display

--- dashboard/app.py
+++ dashboard/app.py
@@ -18,9 +18,16 @@ demoOptionData = pd.DataFrame({
     "strike": [30000, 35000, 40000, 45000, 50000],
     "type": ["call", "call", "call", "put", "put"],
     "expiry": ["1M", "1M", "2w", "1w", "1w"],
+    "expiryVals": [2, 2, 1, 0, 0],
     "price": [1500, 1200, 800, 600, 400],
     "IV" : [0.4, 0.45, 0.4, 0.35, 0.3]
 })
+
+demoOptionList = [
+    [600, 400], # 1w
+    [800, 900],      # 2w
+    [1500, 1200] # 1M
+]
 
 demoCoinData = pd.DataFrame({
     "price": np.random.rand(100).cumsum() * 40000,
@@ -43,11 +50,11 @@ with col2:
     st.subheader(f"{symbol} Options Data (Max Expiry: {expriry})")
 
     fig, ax2 = plt.subplots()
-    im = ax2.imshow(demoOptionData, cmap='viridis', aspect='auto')
+    im = ax2.imshow(demoOptionList, cmap='viridis', aspect='auto')
 
     ax.set_xlabel("Moneyness")
     ax.set_ylabel("Time to Maturity")
 
-    ax.set_yticks(np.arange(demoOptionData["expiry"].map({"1w":0, "2w":1, "1M":2})))
+    ax.set_yticks(np.arange(demoOptionData["expiryVals"].max()))
     ax.set_xticks(np.arange(len(demoOptionData["strike"])))
     st.pyplot(fig)
