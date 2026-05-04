b6fd414b96b300287b7a5bbc1f0bde5038fbed38
Matthew Chang <matthewchang04@gmail.com>
2026-05-03 22:02:30 -0400

    coinbase client subscribe changes

--- src/CoinbaseClient.cpp
+++ src/CoinbaseClient.cpp
@@ -47,7 +47,7 @@ void CoinbaseClient::subscribe_ticker(const std::string& symbol) {
     std::string subReq = fmt::format(subTemplate, symbol);
 
     ws_->async_write(net::buffer(subReq));
-}
+} 
 
 void CoinbaseClient::unsubscribe_ticker(const std::string& symbol) {
     if (!beast::get_lowest_layer(*ws_).is_open()) {
