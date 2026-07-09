6b94fd8dfb8da4b3b18e8fdc2e2ab3b29c556039
Matthew Chang <matthewchang04@gmail.com>
2026-07-08 23:04:10 -0400

    added scaffolding for internal analytics

--- include/AnalyticsEngine.hpp
+++ include/AnalyticsEngine.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include "DataContainers.hpp"
 
 class RollingVWAP {
     
@@ -18,10 +20,42 @@ class OrderbookMetrics {
     double getImbalance();
 };
 
+struct TradeMetrics {
+    double lastTick_;
+    double buyVolumne_;
+    double sellVolume_;
+    int tradesLastMinute_;
+};
+
+class InstrumentSnapshot {
+    private:
+        double lastTick_;
+        double buyVolume_;
+        double sellVolume_;
+        int tradesLastMinute_;
+
+        double orderbookImbalance_;
+
+
+        double vol30s_;
+        double vol5m_;
+        double vwap_;
+
+    public:
+        OrderbookMetrics orderbook_;
+        SpreadMetrics spread_; 
+};
+
 class AnalyticsEngine {
     private:
         double getReturns(const std::string& product);
+        InstrumentSnapshot snapshot_;
 
+    public:
 
+        getSnap(const SpotTick& tick);
+        RollingVol vol30s_;
+        RollingVol vol5m_;
+        RollingVWAP vwap_;
 
 };
